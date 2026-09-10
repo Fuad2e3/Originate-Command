9a19640d7422b110b86eeea3e5623bb698a56b89
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-09-10 19:42:16 +0600

    style: calibrate full Day and Night mode contrast for all orange banners and client cards

--- assets/pwa/sw.js
+++ assets/pwa/sw.js
@@ -4,7 +4,7 @@
    Location: assets/pwa/sw.js
    ========================================================================= */
 
-const CACHE_NAME = 'oc-pwa-cache-v2.11.76';
+const CACHE_NAME = 'oc-pwa-cache-v2.11.77';
 const ASSETS_TO_CACHE = [
   '../../',
   '../../index.html',
